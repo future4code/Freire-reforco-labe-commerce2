3b2244767d0745cd90a23f886af42fa3d2e9b86a
Sofia Anjos <emmanuelesofia@gmail.com>
2022-08-06 15:15:26 -0300

    modificações de fonte e logo

--- src/components/Header/Logo/AppLogo.jsx
+++ src/components/Header/Logo/AppLogo.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "./AppLogo.css";
 
-/*React Icons*/
-import { GiExplodingPlanet } from "react-icons/gi";
-
 export const AppLogo = () => {
   return (
     <div className="header-logo">
-      <GiExplodingPlanet />
-
       <div className="header-texts">
         <h1>Space-Toys</h1>
         <h5>Trazidos do espaço para sua diversão!</h5>
